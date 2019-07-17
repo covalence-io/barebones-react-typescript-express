4b4d82057672a25c04db19e392a8891d8bb53170
przekwas <luke@covalence.io>
2019-07-17 13:39:51 -0500

    cleaning up server boilerplate code

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,16 +1,10 @@
-import * as path from 'path';
 import * as express from 'express';
 import apiRouter from './routes';
 
 const app = express();
 
-let p = path.join(__dirname, '../public');
-console.log(p);
-
-app.use(express.static(p));
+app.use(express.static('public'));
 app.use(apiRouter);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`);
-});
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
