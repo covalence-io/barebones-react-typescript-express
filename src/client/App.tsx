e0e9d50b459a9b48ec4d87f1b940be57325ead96
przekwas <luke@covalence.io>
2019-08-09 10:33:12 -0500

    adjusting starter code to show bootstrap override

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,35 +1,37 @@
 import * as React from 'react';
 
-import './scss/app';
-
-export default class App extends React.Component<IAppProps, IAppState> {
+class App extends React.Component<IAppProps, IAppState> {
 
     constructor(props: IAppProps) {
         super(props);
-
-        this.state = { name: null };
+        this.state = {
+            name: null
+        };
     }
 
     async componentWillMount() {
-        let r = await fetch('/api/hello');
-        let name = await r.json();
-        this.setState({ name })
+        try {
+            let r = await fetch('/api/hello');
+            let name = await r.json();
+            this.setState({ name });
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    render () {
+    render() {
         return (
-            <main className="container">
-                <h1 className="covalence-blue">Hello {this.state.name}!</h1>
-                <h2></h2>
+            <main className="container my-5">
+                <h1 className="text-primary text-center">Hello {this.state.name}!</h1>
             </main>
         )
     }
 }
 
-interface IAppProps {
+export interface IAppProps { }
 
-}
-
-interface IAppState {
+export interface IAppState {
     name: string;
 }
+
+export default App;
