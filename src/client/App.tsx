1bfd49cf78a22b4c8cbd41c68129d1a72d2392b2
przekwas <luke@covalence.io>
2019-11-05 09:48:55 -0600

    changing lifecycle methods because old one is deprecating

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,37 +1,36 @@
 import * as React from 'react';
 
 class App extends React.Component<IAppProps, IAppState> {
+	constructor(props: IAppProps) {
+		super(props);
+		this.state = {
+			name: null
+		};
+	}
 
-    constructor(props: IAppProps) {
-        super(props);
-        this.state = {
-            name: null
-        };
-    }
+	async componentDidMount() {
+		try {
+			let r = await fetch('/api/hello');
+			let name = await r.json();
+			this.setState({ name });
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-    async componentWillMount() {
-        try {
-            let r = await fetch('/api/hello');
-            let name = await r.json();
-            this.setState({ name });
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    render() {
-        return (
-            <main className="container my-5">
-                <h1 className="text-primary text-center">Hello {this.state.name}!</h1>
-            </main>
-        )
-    }
+	render() {
+		return (
+			<main className="container my-5">
+				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
+			</main>
+		);
+	}
 }
 
-export interface IAppProps { }
+export interface IAppProps {}
 
 export interface IAppState {
-    name: string;
+	name: string;
 }
 
 export default App;
