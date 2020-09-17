be0fad41171425e65dd8a75b7de43b2a375ac88d
przekwas <luke@covalence.io>
2020-09-17 14:57:38 -0500

    updating to use hook example

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,36 +1,27 @@
 import * as React from 'react';
 
-class App extends React.Component<IAppProps, IAppState> {
-	constructor(props: IAppProps) {
-		super(props);
-		this.state = {
-			name: null
-		};
-	}
+const App = (props: AppProps) => {
+	const [greeting, setGreeting] = React.useState<string>('');
 
-	async componentDidMount() {
-		try {
-			let r = await fetch('/api/hello');
-			let name = await r.json();
-			this.setState({ name });
-		} catch (error) {
-			console.log(error);
-		}
-	}
+	React.useEffect(() => {
+		(async () => {
+			try {
+				const res = await fetch('/api/sup');
+				const greeting = await res.json();
+				setGreeting(greeting);
+			} catch (error) {
+				console.log(error);
+			}
+		})();
+	}, []);
 
-	render() {
-		return (
-			<main className="container my-5">
-				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
-			</main>
-		);
-	}
-}
+	return (
+		<div className="min-vh-100 d-flex justify-content-center align-items-center">
+			<h1 className="display-1">Sup {greeting}!</h1>
+		</div>
+	);
+};
 
-export interface IAppProps {}
-
-export interface IAppState {
-	name: string;
-}
+interface AppProps {}
 
 export default App;
