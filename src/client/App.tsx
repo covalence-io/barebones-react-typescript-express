4506f2e25807add5370d0164a285bfbb1c3b0ef6
przekwas <luke@covalence.io>
2020-11-05 11:03:07 -0600

    reverting to class to match lectures and leaving hook example commented

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,27 +1,63 @@
 import * as React from 'react';
 
-const App = (props: AppProps) => {
-	const [greeting, setGreeting] = React.useState<string>('');
+class App extends React.Component<IAppProps, IAppState> {
+	constructor(props: IAppProps) {
+		super(props);
+		this.state = {
+			name: null
+		};
+	}
 
-	React.useEffect(() => {
-		(async () => {
-			try {
-				const res = await fetch('/api/sup');
-				const greeting = await res.json();
-				setGreeting(greeting);
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, []);
+	async componentDidMount() {
+		try {
+			let r = await fetch('/api/hello');
+			let name = await r.json();
+			this.setState({ name });
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-	return (
-		<div className="min-vh-100 d-flex justify-content-center align-items-center">
-			<h1 className="display-1">Sup {greeting}!</h1>
-		</div>
-	);
-};
+	render() {
+		return (
+			<main className="container my-5">
+				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
+			</main>
+		);
+	}
+}
 
-interface AppProps {}
+export interface IAppProps {}
+
+export interface IAppState {
+	name: string;
+}
 
 export default App;
+
+//
+// const App = (props: AppProps) => {
+// 	const [greeting, setGreeting] = React.useState<string>('');
+
+// 	React.useEffect(() => {
+// 		(async () => {
+// 			try {
+// 				const res = await fetch('/api/hello');
+// 				const greeting = await res.json();
+// 				setGreeting(greeting);
+// 			} catch (error) {
+// 				console.log(error);
+// 			}
+// 		})();
+// 	}, []);
+
+// 	return (
+// 		<div className="min-vh-100 d-flex justify-content-center align-items-center">
+// 			<h1 className="display-1">Hello {greeting}!</h1>
+// 		</div>
+// 	);
+// };
+
+// interface AppProps {}
+
+// export default App;
