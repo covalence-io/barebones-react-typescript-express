8e6303e2b3227598ba3f8410faf4709950b62f0d
przekwas <luke@covalence.io>
2021-03-18 23:24:49 -0500

    updating boiler code to showcase react hooks example

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,63 +1,62 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 
-class App extends React.Component<IAppProps, IAppState> {
-	constructor(props: IAppProps) {
-		super(props);
-		this.state = {
-			name: null
-		};
-	}
+const App = (props: AppProps) => {
+	const [greeting, setGreeting] = useState<string>('');
 
-	async componentDidMount() {
-		try {
-			let r = await fetch('/api/hello');
-			let name = await r.json();
-			this.setState({ name });
-		} catch (error) {
-			console.log(error);
+	useEffect(() => {
+		async function getGreeting() {
+			try {
+				const res = await fetch('/api/hello');
+				const greeting = await res.json();
+				setGreeting(greeting);
+			} catch (error) {
+				console.log(error);
+			}
 		}
-	}
+		getGreeting();
+	}, []);
 
-	render() {
-		return (
-			<main className="container my-5">
-				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
-			</main>
-		);
-	}
-}
+	return (
+		<main className="container my-5">
+			<h1 className="text-primary text-center">Hello {greeting}!</h1>
+		</main>
+	);
+};
 
-export interface IAppProps {}
+interface AppProps {}
 
-export interface IAppState {
-	name: string;
-}
+// class App extends React.Component<IAppProps, IAppState> {
+// 	constructor(props: IAppProps) {
+// 		super(props);
+// 		this.state = {
+// 			name: null
+// 		};
+// 	}
+
+// 	async componentDidMount() {
+// 		try {
+// 			let r = await fetch('/api/hello');
+// 			let name = await r.json();
+// 			this.setState({ name });
+// 		} catch (error) {
+// 			console.log(error);
+// 		}
+// 	}
+
+// 	render() {
+// 		return (
+// 			<main className="container my-5">
+// 				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
+// 			</main>
+// 		);
+// 	}
+// }
+
+// export interface IAppProps {}
+
+// export interface IAppState {
+// 	name: string;
+// }
 
 export default App;
-
-//
-// const App = (props: AppProps) => {
-// 	const [greeting, setGreeting] = React.useState<string>('');
-
-// 	React.useEffect(() => {
-// 		(async () => {
-// 			try {
-// 				const res = await fetch('/api/hello');
-// 				const greeting = await res.json();
-// 				setGreeting(greeting);
-// 			} catch (error) {
-// 				console.log(error);
-// 			}
-// 		})();
-// 	}, []);
-
-// 	return (
-// 		<div className="min-vh-100 d-flex justify-content-center align-items-center">
-// 			<h1 className="display-1">Hello {greeting}!</h1>
-// 		</div>
-// 	);
-// };
-
-// interface AppProps {}
-
-// export default App;
